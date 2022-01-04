79a48545c2e5f8c22fe659fc0510cca06bf02aac
Roger Chen <mengqiu.chen@hotmail.com>
2022-01-04 11:19:39 -0700

    wire bean of BCryptPasswordEncoder

--- src/main/java/com/myblog/myblog/MyblogApplication.java
+++ src/main/java/com/myblog/myblog/MyblogApplication.java
@@ -2,9 +2,16 @@ package com.myblog.myblog;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
+import org.springframework.context.annotation.Bean;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 
-@SpringBootApplication
+@SpringBootApplication(exclude = {SecurityAutoConfiguration.class})
 public class MyblogApplication {
+	@Bean
+	public BCryptPasswordEncoder bCryptPasswordEncoder() {
+		return new BCryptPasswordEncoder();
+	}
 
 	public static void main(String[] args) {
 		SpringApplication.run(MyblogApplication.class, args);
