06516e3b97a6399be463879de159ca5a1c3183ec
Roger Chen <mengqiu.chen@hotmail.com>
2022-01-04 17:01:37 -0700

    Create LoginRequest

---
+++ src/main/java/com/myblog/myblog/request/LoginRequest.java
@@ -0,0 +1,26 @@
+package com.myblog.myblog.request;
+
+import com.fasterxml.jackson.annotation.JsonProperty;
+
+public class LoginRequest {
+    @JsonProperty
+    private String username;
+    @JsonProperty
+    private String password;
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+}
