79f441d65022985c16d72e7c47c3920269768d55
Roger Chen <mengqiu.chen@hotmail.com>
2022-01-05 19:55:56 -0700

    Create schema

---
+++ src/main/resources/schema.sql
@@ -0,0 +1,148 @@
+
+SET FOREIGN_KEY_CHECKS=0;
+
+-- ----------------------------
+-- Table structure for archives
+-- ----------------------------
+DROP TABLE IF EXISTS `archives`;
+CREATE TABLE `archives` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `archiveName` varchar(255) NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=4 DEFAULT CHARSET=utf8;
+
+-- ----------------------------
+-- Table structure for article
+-- ----------------------------
+DROP TABLE IF EXISTS `post`;
+CREATE TABLE `post` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `postId` bigint(20) NOT NULL,
+  `title` varchar(255) NOT NULL,
+  `content` longtext NOT NULL,
+  `tag` varchar(255) NOT NULL,
+  `type` varchar(255) NOT NULL,
+  `category` varchar(255) NOT NULL,
+  `publishDate` varchar(255) NOT NULL,
+  `updateDate` varchar(255) NOT NULL,
+  `articleTabloid` text NOT NULL,
+  `lastArticleId` bigint(20) DEFAULT NULL,
+  `nextArticleId` bigint(20) DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=20 DEFAULT CHARSET=utf8;
+
+-- ----------------------------
+-- Table structure for categories
+-- ----------------------------
+DROP TABLE IF EXISTS `categories`;
+CREATE TABLE `categories` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `categoryName` varchar(255) NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=8 DEFAULT CHARSET=utf8;
+
+-- ----------------------------
+-- Table structure for comment_record
+-- ----------------------------
+DROP TABLE IF EXISTS `comment_record`;
+CREATE TABLE `comment_record` (
+  `id` bigint(11) NOT NULL AUTO_INCREMENT,
+  `pId` bigint(20) NOT NULL,
+  `articleId` bigint(20) NOT NULL,
+  `answererId` int(11) NOT NULL,
+  `respondentId` int(11) NOT NULL,
+  `commentDate` varchar(255) NOT NULL,
+  `commentContent` text NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=16 DEFAULT CHARSET=utf8;
+
+-- ----------------------------
+-- Table structure for feedback
+-- ----------------------------
+DROP TABLE IF EXISTS `feedback`;
+CREATE TABLE `feedback` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `feedbackContent` text NOT NULL,
+  `contactInfo` varchar(255) DEFAULT NULL,
+  `personId` int(11) NOT NULL,
+  `feedbackDate` varchar(255) NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+-- ----------------------------
+-- Table structure for leave_message_record
+-- ----------------------------
+DROP TABLE IF EXISTS `leave_message_record`;
+CREATE TABLE `leave_message_record` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `pageName` varchar(255) NOT NULL,
+  `pId` int(255) NOT NULL,
+  `answererId` int(11) NOT NULL,
+  `respondentId` int(11) NOT NULL,
+  `leaveMessageDate` varchar(255) NOT NULL,
+  `likes` int(11) NOT NULL,
+  `leaveMessageContent` text NOT NULL,
+  `isRead` tinyint(1) NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=19 DEFAULT CHARSET=utf8;
+
+-- ----------------------------
+-- Table structure for role
+-- ----------------------------
+DROP TABLE IF EXISTS `role`;
+CREATE TABLE `role` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `name` varchar(255) NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=4 DEFAULT CHARSET=utf8;
+
+-- ----------------------------
+-- Records of role
+-- ----------------------------
+INSERT INTO `role` VALUES ('1', 'ROLE_USER');
+INSERT INTO `role` VALUES ('2', 'ROLE_ADMIN');
+
+-- ----------------------------
+-- Table structure for tags
+-- ----------------------------
+DROP TABLE IF EXISTS `tags`;
+CREATE TABLE `tags` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `tagName` varchar(255) NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=19 DEFAULT CHARSET=utf8;
+
+-- ----------------------------
+-- Table structure for user
+-- ----------------------------
+DROP TABLE IF EXISTS `user`;
+CREATE TABLE `user` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `username` varchar(255) NOT NULL,
+  `password` varchar(255) NOT NULL,
+  `email` varchar(255) DEFAULT NULL,
+  `avatarImgUrl` text NOT NULL,
+  `recentlyLanded` varchar(255) DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=16 DEFAULT CHARSET=utf8;
+
+-- ----------------------------
+-- Table structure for user_role
+-- ----------------------------
+DROP TABLE IF EXISTS `user_role`;
+CREATE TABLE `user_role` (
+  `User_id` int(11) NOT NULL,
+  `Role_id` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+----------------------------
+-- Table structure for visitor
+-- ----------------------------
+DROP TABLE IF EXISTS `visitor`;
+CREATE TABLE `visitor` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `visitorNum` bigint(20) NOT NULL,
+  `pageName` text NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=22 DEFAULT CHARSET=utf8;
+
