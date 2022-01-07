f4ee48aed94f0aff468ceef40785b7f5edc814fe
Roger Chen <mengqiu.chen@hotmail.com>
2022-01-06 17:23:56 -0700

    reduce expiration_time

--- src/main/java/com/myblog/myblog/config/SecurityConstants.java
+++ src/main/java/com/myblog/myblog/config/SecurityConstants.java
@@ -3,8 +3,8 @@ package com.myblog.myblog.config;
 public class SecurityConstants {
 
 	public static final String SECRET = "oursecretkey";
-    public static final long EXPIRATION_TIME = 864_000_000; // 10 days
+    public static final long EXPIRATION_TIME = 864_00_000; // 1day
     public static final String TOKEN_PREFIX = "Bearer ";
     public static final String HEADER_STRING = "Authorization";
-    public static final String SIGN_UP_URL = "/user/create";
+    public static final String SIGN_UP_URL = "/user/create/**";
 }
