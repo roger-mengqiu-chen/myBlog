a92fce14a8585e3a3c24757ac20b14033c75346a
Roger Chen <mengqiu.chen@hotmail.com>
2022-01-06 17:39:57 -0700

    Add delete user Add create admin for testing

--- src/main/java/com/myblog/myblog/controller/UserController.java
+++ src/main/java/com/myblog/myblog/controller/UserController.java
@@ -5,7 +5,10 @@ import com.myblog.myblog.request.CreateUserRequest;
 import com.myblog.myblog.request.ModifyUserRequest;
 import com.myblog.myblog.service.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.web.bind.annotation.*;
 
@@ -18,18 +21,54 @@ public class UserController {
     private BCryptPasswordEncoder encoder;
 
     @PostMapping("/create")
-    public ResponseEntity<User> createUser (@RequestBody CreateUserRequest createUserRequest) {
+    public ResponseEntity createUser (@RequestBody CreateUserRequest createUserRequest) {
         String username = createUserRequest.getUsername();
         String password = createUserRequest.getPassword();
         String email = createUserRequest.getEmail();
         String avatarUrl = createUserRequest.getAvatarUrl();
+        User existedUser = userService.findUserByUsername(username);
+        if (existedUser != null) {
+            return ResponseEntity.badRequest().body("User existed!");
+        }
         User user = new User();
         user.setUsername(username);
         user.setPassword(encoder.encode(password));
         user.setEmail(email);
         user.setAvatarUrl(avatarUrl);
+
         try {
-            userService.createUser(user);
+            userService.createUser(user, 1);
+            return ResponseEntity.ok(user);
+        } catch (Exception e) {
+            e.printStackTrace();
+            return ResponseEntity.badRequest().build();
+        }
+    }
+
+    /**
+     * For testing
+     * TODO to be removed
+     * @param createUserRequest
+     * @return
+     */
+    @PostMapping("/create/admin")
+    public ResponseEntity createAdmin(@RequestBody CreateUserRequest createUserRequest) {
+        String username = createUserRequest.getUsername();
+        String password = createUserRequest.getPassword();
+        String email = createUserRequest.getEmail();
+        String avatarUrl = createUserRequest.getAvatarUrl();
+        User existedUser = userService.findUserByUsername(username);
+        if (existedUser != null) {
+            return ResponseEntity.badRequest().body("User existed!");
+        }
+        User user = new User();
+        user.setUsername(username);
+        user.setPassword(encoder.encode(password));
+        user.setEmail(email);
+        user.setAvatarUrl(avatarUrl);
+
+        try {
+            userService.createUser(user, 2);
             return ResponseEntity.ok(user);
         } catch (Exception e) {
             e.printStackTrace();
@@ -52,4 +91,22 @@ public class UserController {
         User user = userService.modifyUser(userId, username, encoder.encode(password), email,avatarUrl);
         return user == null ? ResponseEntity.notFound().build() : ResponseEntity.ok(user);
     }
+
+    @GetMapping("/delete/{username}")
+    public ResponseEntity deleteUser(@PathVariable String username) {
+        Authentication auth = SecurityContextHolder.getContext().getAuthentication();
+        System.out.println("Authorities: ");
+        auth.getAuthorities().forEach(System.out::println);
+        if (auth != null && (auth.getAuthorities().stream().anyMatch(a -> a.getAuthority().equals("ROLE_ADMIN")))
+                                || ((auth.getAuthorities().stream().anyMatch(a -> a.getAuthority().equals("ROLE_USER")))
+                                        && auth.getName().equals(username))) {
+            userService.deleteUser(username);
+            return ResponseEntity.ok().build();
+        }
+        else {
+            System.out.println(auth.getAuthorities().size());
+            return ResponseEntity.status(401).build();
+        }
+    }
+
 }
