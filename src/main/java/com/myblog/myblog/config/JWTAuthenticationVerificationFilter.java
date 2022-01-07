2fdb20d9d92f50954a6e5f2e68ee9fa45583b447
Roger Chen <mengqiu.chen@hotmail.com>
2022-01-06 20:59:27 -0700

    Parse username and role of JWT token

--- src/main/java/com/myblog/myblog/config/JWTAuthenticationVerificationFilter.java
+++ src/main/java/com/myblog/myblog/config/JWTAuthenticationVerificationFilter.java
@@ -1,11 +1,12 @@
 package com.myblog.myblog.config;
 
 import com.auth0.jwt.JWT;
+import com.auth0.jwt.interfaces.DecodedJWT;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.security.web.authentication.www.BasicAuthenticationFilter;
-import org.springframework.stereotype.Component;
 
 import javax.servlet.FilterChain;
 import javax.servlet.ServletException;
@@ -13,15 +14,15 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
 import java.util.ArrayList;
-
+import java.util.List;
 import static com.auth0.jwt.algorithms.Algorithm.HMAC512;
-@Component
+
 public class JWTAuthenticationVerificationFilter extends BasicAuthenticationFilter {
-	
+
 	public JWTAuthenticationVerificationFilter(AuthenticationManager authManager) {
         super(authManager);
     }
-	
+
 	@Override
     protected void doFilterInternal(HttpServletRequest req, HttpServletResponse res, FilterChain chain) 
     		throws IOException, ServletException {
@@ -38,6 +39,7 @@ public class JWTAuthenticationVerificationFilter extends BasicAuthenticationFilt
         } catch (Exception e) {
             //TODO
             // log unauthorized access
+            e.printStackTrace();
         }
         chain.doFilter(req, res);
     }
@@ -45,11 +47,16 @@ public class JWTAuthenticationVerificationFilter extends BasicAuthenticationFilt
 	private UsernamePasswordAuthenticationToken getAuthentication(HttpServletRequest req) {
 		String token = req.getHeader(SecurityConstants.HEADER_STRING);
         if (token != null) {
-            String user = JWT.require(HMAC512(SecurityConstants.SECRET.getBytes())).build()
-                    .verify(token.replace(SecurityConstants.TOKEN_PREFIX, ""))
-                    .getSubject();
+            DecodedJWT jwt = JWT.require(HMAC512(SecurityConstants.SECRET.getBytes())).build()
+                    .verify(token.replace(SecurityConstants.TOKEN_PREFIX, ""));
+
+            String user = jwt.getClaim("username").asString();
+            String role = jwt.getClaim("role").asString();
+            List<SimpleGrantedAuthority> authorities = new ArrayList<>();
+            authorities.add(new SimpleGrantedAuthority(role));
+
             if (user != null) {
-                return new UsernamePasswordAuthenticationToken(user, null, new ArrayList<>());
+                return new UsernamePasswordAuthenticationToken(user, null, authorities);
             }
             return null;
         }
