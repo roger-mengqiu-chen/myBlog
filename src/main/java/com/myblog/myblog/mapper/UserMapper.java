f8bf1610f12151ae18abfa6e1c538bb0817a960d
Roger Chen <mengqiu.chen@hotmail.com>
2022-01-06 17:38:04 -0700

    Involve role of users

--- src/main/java/com/myblog/myblog/mapper/UserMapper.java
+++ src/main/java/com/myblog/myblog/mapper/UserMapper.java
@@ -10,13 +10,27 @@ public interface UserMapper {
             "WHERE username = #{username}")
     User findUserByUsername(String username);
 
+    @Select("SELECT id FROM user " +
+            "WHERE username = #{username}")
+    int findyUserId(String username);
+
     @Insert("INSERT INTO user (username, password, email, avatarImgUrl) " +
             "VALUES (#{username}, #{password}, #{email}, #{avatarUrl})")
     void save(User user);
 
+    @Insert("INSERT INTO user_role (user_id, role_id) " +
+            "VALUES (#{userId}, #{roleId})")
+    void saveRole(@Param("userId") int userId, @Param("roleId") int roleId);
+
     @Select("SELECT * FROM user " +
             "WHERE id = #{id}")
     User findyUserById(int id);
+
+    @Select("SELECT name FROM role r JOIN user_role ur JOIN user u " +
+            "WHERE r.id = ur.role_id " +
+            "   AND ur.user_id = u.id " +
+            "   AND u.username = #{username}")
+    String findUserRole(@Param("username") String username);
 
     @Update("UPDATE user " +
             "SET username = #{username}, " +
