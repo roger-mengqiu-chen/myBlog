791570d9f53f24ea6fab23b9161c7de70d0199f9
Roger Chen <mengqiu.chen@hotmail.com>
2022-01-08 14:42:14 -0700

    Feedback CRUD

---
+++ src/main/java/com/myblog/myblog/mapper/FeedbackMapper.java
@@ -0,0 +1,34 @@
+package com.myblog.myblog.mapper;
+
+import com.myblog.myblog.entity.Feedback;
+import org.apache.ibatis.annotations.*;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+
+@Mapper
+@Repository
+public interface FeedbackMapper {
+    /* Create */
+    @Insert("INSERT INTO feedback (feedbackContent, email, feedbackDate) " +
+            "VALUES (#{feedbackContent}, #{email}, #{feedbackDate})")
+    int saveFeedback(Feedback feedback);
+
+    /* Read */
+    @Select("SELECT * FROM feedback")
+    @Results({
+            @Result(id = true, property = "feedbackId", column = "feedbackId"),
+            @Result(property = "feedbackContent", column = "feedbackContent"),
+            @Result(property = "email", column = "email"),
+            @Result(property = "feedbackDate", column = "feedbackDate")
+    })
+    List<Feedback> getAllFeedback();
+
+    /* Update */
+    /* Sending feedback doesn't require registration. Once feedback is sent, there's no way to update it */
+
+    /* Delete */
+    @Delete("DELETE FROM feedback " +
+            "WHERE feedbackId = #{feedbackId}")
+    int deleteFeedback(Feedback feedback);
+}
