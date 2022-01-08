4bf9dbe8fb05102b7935ddfdd185d4a66e03cf9c
Roger Chen <mengqiu.chen@hotmail.com>
2022-01-08 12:22:06 -0700

    Change return type to JsonResponse

--- src/main/java/com/myblog/myblog/controller/UserController.java
+++ src/main/java/com/myblog/myblog/controller/UserController.java
@@ -1,10 +1,10 @@
 package com.myblog.myblog.controller;
 
-import com.myblog.myblog.entity.User;
+import com.myblog.myblog.constant.Status;
 import com.myblog.myblog.request.ModifyUserRequest;
+import com.myblog.myblog.response.JsonResponse;
 import com.myblog.myblog.service.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.ResponseEntity;
 import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.authority.SimpleGrantedAuthority;
@@ -25,34 +25,31 @@ public class UserController {
     private BCryptPasswordEncoder encoder;
 
     @GetMapping("/{username}")
-    public ResponseEntity<User> getUserByUsername (@PathVariable String username) {
-        User user = userService.findUserByUsername(username);
-        return user == null ? ResponseEntity.notFound().build() : ResponseEntity.ok(user);
+    public JsonResponse getUserByUsername (@PathVariable String username) {
+        return userService.findUserByUsername(username);
     }
 
     @PostMapping("/modify/{username}")
-    public ResponseEntity<User> modifyUser(@PathVariable String username, @RequestBody ModifyUserRequest modifyUserRequest) {
+    public JsonResponse modifyUser(@PathVariable String username, @RequestBody ModifyUserRequest modifyUserRequest) {
         int userId = userService.getUserIdByName(username);
         String name = modifyUserRequest.getUsername();
         String email = modifyUserRequest.getEmail();
         String password = modifyUserRequest.getPassword();
         String avatarUrl = modifyUserRequest.getAvatarUrl();
-        User user = userService.modifyUser(userId, name, encoder.encode(password), email,avatarUrl);
-        return user == null ? ResponseEntity.notFound().build() : ResponseEntity.ok(user);
+        return userService.modifyUser(userId, name, encoder.encode(password), email,avatarUrl);
     }
 
     @GetMapping("/delete/{username}")
-    public ResponseEntity deleteUser(@PathVariable String username) {
+    public JsonResponse deleteUser(@PathVariable String username) {
         Authentication auth = SecurityContextHolder.getContext().getAuthentication();
         List<SimpleGrantedAuthority> roles = new ArrayList(auth.getAuthorities());
         String role = roles.get(0).getAuthority();
         // USER can only delete itself, ADMIN can delete any user
         if (auth.getName().equals(username) || role.equals("ROLE_ADMIN")) {
-            userService.deleteUser(username);
-            return ResponseEntity.ok().build();
+            return userService.deleteUser(username);
         }
         else {
-            return ResponseEntity.status(401).build();
+            return new JsonResponse(Status.PERMISSION_DENIED);
         }
     }
 
