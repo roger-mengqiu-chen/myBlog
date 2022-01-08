d3165692d2efa9e08d6285980df1c6dc5ff6042a
Roger Chen <mengqiu.chen@hotmail.com>
2022-01-08 13:26:50 -0700

    Tag CRUD

--- src/main/java/com/myblog/myblog/mapper/TagMapper.java
+++ src/main/java/com/myblog/myblog/mapper/TagMapper.java
@@ -1,7 +1,7 @@
 package com.myblog.myblog.mapper;
 
-import org.apache.ibatis.annotations.Mapper;
-import org.apache.ibatis.annotations.Select;
+import com.myblog.myblog.entity.Tag;
+import org.apache.ibatis.annotations.*;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
@@ -9,8 +9,31 @@ import java.util.List;
 @Mapper
 @Repository
 public interface TagMapper {
+    /* Create */
+    @Insert("INSERT INTO tags (tagName) " +
+            "VALUES (#{name})")
+    int createTag(String name);
 
+    /* Read */
     @Select("SELECT tagName FROM tags")
     List<String> getAllTags();
 
+    @Select("SELECT * FROM tags " +
+            "WHERE tagName = #{name}")
+    @Results({
+            @Result(id = true, property = "tagId", column = "tagId"),
+            @Result(property = "tagName", column = "tagName")
+    })
+    Tag findTagByName (String name);
+
+    /* Update */
+    @Update("UPDATE tags " +
+            "SET tagName = #{tagName} " +
+            "WHERE tagId = #{tagId}")
+    int updateTag(Tag tag);
+
+    /* Delete */
+    @Delete("DELETE FROM tags " +
+            "WHERE tagId = #{tagId}")
+    int deleteTag(Tag tag);
 }
