e67322125e5e12d11ec5928123cdf68c666584b0
Roger Chen <mengqiu.chen@hotmail.com>
2022-01-14 16:47:16 -0700

    Create CategoryController

---
+++ src/main/java/com/myblog/myblog/controller/CategoryController.java
@@ -0,0 +1,21 @@
+package com.myblog.myblog.controller;
+
+import com.myblog.myblog.response.JsonResponse;
+import com.myblog.myblog.service.CategoryService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * This is open to public. Only has 1 method
+ */
+@RestController
+@RequestMapping("/category")
+public class CategoryController {
+    @Autowired
+    private CategoryService categoryService;
+
+    public JsonResponse getAllCategories() {
+        return categoryService.getAllCategories();
+    }
+}
