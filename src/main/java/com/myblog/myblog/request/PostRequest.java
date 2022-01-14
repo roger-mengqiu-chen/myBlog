2dd838bdf4ed23303e56940e2fa764526205c059
Roger Chen <mengqiu.chen@hotmail.com>
2022-01-14 16:54:29 -0700

    Create PostRequest

---
+++ src/main/java/com/myblog/myblog/request/PostRequest.java
@@ -0,0 +1,25 @@
+package com.myblog.myblog.request;
+
+import com.fasterxml.jackson.annotation.JsonProperty;
+import lombok.Data;
+
+import java.time.LocalDate;
+import java.util.List;
+
+@Data
+public class PostRequest {
+    @JsonProperty
+    private Integer postId;
+    @JsonProperty
+    private String title;
+    @JsonProperty
+    private String content;
+    @JsonProperty
+    private String excerpt;
+    @JsonProperty
+    private String category;
+    @JsonProperty
+    private LocalDate publishDate;
+    @JsonProperty
+    private List<String> tags;
+}
