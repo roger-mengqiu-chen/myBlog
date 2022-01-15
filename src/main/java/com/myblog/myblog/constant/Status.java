9bcdc2dcda174e31b173c707de6feba23631e06b
Roger Chen <mengqiu.chen@hotmail.com>
2022-01-14 22:26:51 -0700

    Add Category and Tag status code

--- src/main/java/com/myblog/myblog/constant/Status.java
+++ src/main/java/com/myblog/myblog/constant/Status.java
@@ -11,6 +11,13 @@ public enum Status {
     PERMISSION_DENIED(403, "Permission denied"),
 
     SERVER_ERROR(500, "Server internal error"),
+
+    TAG_NOT_FOUND(600, "Tag not found"),
+    TAG_EXIST(601, "Tag existed"),
+    TAGS_PARTIALLY_CREATED(602, "Tags are partially created"),
+
+    CATEGORY_NOT_FOUND(700, "Category not found"),
+    CATEGORY_EXIST(701, "Category exists")
     ;
 
     private final int code;
