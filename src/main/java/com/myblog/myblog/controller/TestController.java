2371d30b84c6dd0b085adec9736f013e078846f0
Roger Chen <mengqiu.chen@hotmail.com>
2022-01-15 18:11:02 -0700

    Add TestController for testing purpose

---
+++ src/main/java/com/myblog/myblog/controller/TestController.java
@@ -0,0 +1,46 @@
+package com.myblog.myblog.controller;
+
+import com.myblog.myblog.entity.User;
+import com.myblog.myblog.request.CreateUserRequest;
+import com.myblog.myblog.response.JsonResponse;
+import com.myblog.myblog.service.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * For test purpose
+ * TODO remove
+ */
+@RestController
+@RequestMapping("/test")
+public class TestController {
+    @Autowired
+    private UserService userService;
+    @Autowired
+    private BCryptPasswordEncoder encoder;
+
+    @PostMapping("/createAdmin")
+    public JsonResponse createAdmin(@RequestBody CreateUserRequest createUserRequest) {
+        String username = createUserRequest.getUsername();
+        String password = createUserRequest.getPassword();
+        String email = createUserRequest.getEmail();
+        String avatarUrl = createUserRequest.getAvatarUrl();
+
+        User user = new User();
+        user.setUsername(username);
+        user.setPassword(encoder.encode(password));
+        user.setEmail(email);
+        user.setAvatarUrl(avatarUrl);
+        user.setRoleId(2);
+
+        return userService.createUser(user);
+    }
+
+
+}
