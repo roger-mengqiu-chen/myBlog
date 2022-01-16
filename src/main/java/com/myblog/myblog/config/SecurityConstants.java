1a50bcf7174396747a03f344f84dcd507408ba62
Roger Chen <mengqiu.chen@hotmail.com>
2022-01-15 18:25:40 -0700

    No need signup path here

--- src/main/java/com/myblog/myblog/config/SecurityConstants.java
+++ src/main/java/com/myblog/myblog/config/SecurityConstants.java
@@ -6,5 +6,4 @@ public class SecurityConstants {
     public static final long EXPIRATION_TIME = 864_00_000; // 1day
     public static final String TOKEN_PREFIX = "Bearer ";
     public static final String HEADER_STRING = "Authorization";
-    public static final String SIGN_UP_URL = "/register/**";
 }
