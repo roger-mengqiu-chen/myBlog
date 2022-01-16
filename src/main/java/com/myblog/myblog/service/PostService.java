85d731ad6bbfccb00e026460edb13e934a645e87
Roger Chen <mengqiu.chen@hotmail.com>
2022-01-16 10:50:25 -0700

    Add updatePost Add some helper methods for creating post and updating post

--- src/main/java/com/myblog/myblog/service/PostService.java
+++ src/main/java/com/myblog/myblog/service/PostService.java
@@ -17,6 +17,7 @@ import org.springframework.stereotype.Service;
 
 import java.time.LocalDate;
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 
 @Service
@@ -39,66 +40,44 @@ public class PostService {
             return new JsonResponse(Status.POST_TITLE_EXISTED);
         }
 
-        category = category.trim().toLowerCase();
-        // find existedCategory, if doesn't exist, create a new one
-        Category existedCategory = categoryMapper.findCategoryByName(category);
-        if (existedCategory != null) {
-            post.setCategoryId(existedCategory.getCategoryId());
-        }
-        else {
-            categoryMapper.createCategory(category);
-            existedCategory = categoryMapper.findCategoryByName(category);
-            post.setCategoryId(existedCategory.getCategoryId());
-        }
+        Integer categoryId = createCategoryIfNotExist(category);
+        post.setCategoryId(categoryId);
 
         // check the existence of each tag, if it doesn't exist, create a new one. And get ids of all the tags.
-        List<Integer> tagIds = new ArrayList<>();
-        for (String tag : tags) {
-            Tag existedTag = tagMapper.findTagByName(tag);
-            if (existedTag == null) {
-                tagMapper.createTag(tag);
-                Integer id = tagMapper.findTagByName(tag).getTagId();
-                tagIds.add(id);
-            }
-            else {
-                tagIds.add(existedTag.getTagId());
-            }
-        }
+        List<Integer> tagIds = createTagsIfNotExist(tags);
+
+        // get id of last post
         Post lastPost = postMapper.getLatestPost();
         Integer lastPostId = null;
         if (lastPost != null) {
             lastPostId = lastPost.getPostId();
         }
+
+        // save post
         LocalDate date = LocalDate.now();
         post.setTitle(title);
         post.setContent(content);
         post.setPublishDate(date);
         post.setExcerpt(excerpt);
         post.setLastPostId(lastPostId);
-
         try {
             postMapper.savePost(post);
         } catch (Exception e) {
             logger.error(e.getMessage());
             return new JsonResponse(Status.SERVER_ERROR);
         }
-        // update relationship between laste post and current post
+
+        // update relationship between last post and current post
         Post savedPost = postMapper.getPostByTitle(title);
         if (lastPost != null) {
             lastPost.setNextPostId(savedPost.getPostId());
             postMapper.updatePost(lastPost);
         }
 
-        for (Integer id : tagIds) {
-            tagMapper.insertPostTag(savedPost.getPostId(), id);
-        }
+        updateTagsOfPost(savedPost.getPostId(), tagIds);
 
-        // persist archive
-        // archive naming format should be yyyy-mm
-        Archive archive = new Archive();
-        archive.setArchiveName(date.toString().substring(0,7));
-        archive.setPostId(savedPost.getPostId());
-        archiveMapper.save(archive);
+        createArchiveForPost(savedPost);
+
         logger.info("Created a new post");
         return new JsonResponse(Status.SUCCESS, savedPost);
     }
@@ -143,6 +122,28 @@ public class PostService {
         }
     }
 
+    public JsonResponse updatePost(int postId, String title, String content, String excerpt, String category, List<String> tags) {
+        Post post = postMapper.getPostById(postId);
+        if (post == null) {
+            return new JsonResponse(Status.POST_NOT_FOUND);
+        }
+        Post existedPost = postMapper.getPostByTitle(title);
+        if (existedPost.getPostId() != postId) {
+            return new JsonResponse(Status.POST_TITLE_EXISTED);
+        }
+
+        int categoryId = createCategoryIfNotExist(category);
+        List<Integer> tagIds = createTagsIfNotExist(tags);
+        post.setTitle(title);
+        post.setContent(content);
+        post.setExcerpt(excerpt);
+        post.setCategoryId(categoryId);
+
+        updateTagsOfPost(postId, tagIds);
+        postMapper.updatePost(post);
+        return new JsonResponse(Status.SUCCESS, post);
+    }
+
     public JsonResponse deletePost(int postId) {
         Post post = postMapper.getPostById(postId);
         if (post == null) {
@@ -180,5 +181,86 @@ public class PostService {
             logger.info("Post is deleted");
             return new JsonResponse(Status.SUCCESS);
         }
+    }
+
+    /**
+     * Create a new archive for a post
+     * Archvive naming format is yyyy-mm
+     * @param post post to be updated
+     */
+    private void createArchiveForPost(Post post) {
+        Archive archive = new Archive();
+        String name = post.getPublishDate().toString().substring(0,7);
+        int postId = post.getPostId();
+        archive.setPostId(postId);
+        archive.setArchiveName(name);
+        try {
+            archiveMapper.save(archive);
+            logger.info("Created a new archive: {}", name);
+        } catch (Exception e) {
+            logger.error(e.getMessage());
+        }
+    }
+
+    /**
+     * Create a category if doesn't exist
+     * @param category category to be created
+     * @return the id of existed category or created category
+     */
+    private Integer createCategoryIfNotExist(String category) {
+        category = category.trim().toLowerCase();
+        Category existedCategory = categoryMapper.findCategoryByName(category);
+        if (existedCategory != null) {
+            return existedCategory.getCategoryId();
+        }
+        else {
+            categoryMapper.createCategory(category);
+            existedCategory = categoryMapper.findCategoryByName(category);
+            return existedCategory.getCategoryId();
+        }
+    }
+
+    /**
+     * Create tags if they don't exist.
+     * @param tags tags to be created
+     * @return ids of existed tag or created tags
+     */
+    private List<Integer> createTagsIfNotExist(List<String> tags) {
+        List<Integer> tagIds = new ArrayList<>();
+        String[] createdTags = new String[tags.size()];
+        for (int i = 0; i < tags.size(); i ++) {
+            String tag = tags.get(i);
+            Tag existedTag = tagMapper.findTagByName(tag);
+            if (existedTag == null) {
+                tagMapper.createTag(tag);
+                Integer id = tagMapper.findTagByName(tag).getTagId();
+                tagIds.add(id);
+                createdTags[i] = tag;
+            }
+            else {
+                tagIds.add(existedTag.getTagId());
+            }
+        }
+        if (createdTags.length != 0) {
+            Object[] objects = Arrays.stream(createdTags).filter(e -> !e.isEmpty()).toArray();
+            logger.info("Created tags: {}", Arrays.toString(objects));
+        }
+        return tagIds;
+    }
+
+    /**
+     * Update tags of a post
+     * The procedure includes delete all the tags of a post
+     * And attach new tags to the post
+     * This method can also be used for a new post
+     * @param postId post to be updated
+     * @param tagIds new tags to be attached
+     */
+    private void updateTagsOfPost(int postId, List<Integer> tagIds) {
+        tagMapper.deleteTagOfPost(postId);
+        for (int tagId : tagIds) {
+            tagMapper.insertPostTag(postId, tagId);
+        }
+        logger.info("Tags of post {} has been updated", postId);
     }
 }
