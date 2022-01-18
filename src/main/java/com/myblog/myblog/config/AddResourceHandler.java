3e5eff0246b46c6b011430f4a05666b98be3a3f5
Roger Chen <mengqiu.chen@hotmail.com>
2022-01-18 10:33:00 -0700

    Add resourceHandler to handle static resource

--- src/main/java/com/myblog/myblog/config/AddResourceHandler.java
+++ src/main/java/com/myblog/myblog/config/AddResourceHandler.java
@@ -8,6 +8,7 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 public class AddResourceHandler implements WebMvcConfigurer {
     @Override
     public void addResourceHandlers(ResourceHandlerRegistry registry) {
-        registry.addResourceHandler("/**").addResourceLocations("classpath:/static/");
+        registry.addResourceHandler("/static/**")
+                .addResourceLocations("classpath:/static/");
     }
 }
