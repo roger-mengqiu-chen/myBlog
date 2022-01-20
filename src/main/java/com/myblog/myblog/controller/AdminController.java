5bc9f01136a2169323ddc81608c3fbcae97ad39e
Roger Chen <mengqiu.chen@hotmail.com>
2022-01-19 19:34:22 -0700

    Add CRUD for categories, tags, files

--- src/main/java/com/myblog/myblog/controller/AdminController.java
+++ src/main/java/com/myblog/myblog/controller/AdminController.java
@@ -1,18 +1,25 @@
 package com.myblog.myblog.controller;
 
+import ch.qos.logback.core.util.FileUtil;
+import com.myblog.myblog.constant.Status;
+import com.myblog.myblog.entity.Category;
 import com.myblog.myblog.entity.User;
 import com.myblog.myblog.request.PostRequest;
 import com.myblog.myblog.request.UpdateAdminRequest;
 import com.myblog.myblog.response.JsonResponse;
+import com.myblog.myblog.service.CategoryService;
 import com.myblog.myblog.service.PostService;
+import com.myblog.myblog.service.TagService;
 import com.myblog.myblog.service.UserService;
-import org.apache.ibatis.annotations.Update;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.security.access.prepost.PreAuthorize;
-import org.springframework.security.core.parameters.P;
+import org.springframework.util.ResourceUtils;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.multipart.MultipartFile;
 
-import java.time.LocalDate;
+import javax.servlet.http.HttpServletRequest;
+import java.io.File;
+import java.io.IOException;
+import java.util.ArrayList;
 import java.util.List;
 
 @RestController
@@ -22,6 +29,12 @@ public class AdminController {
     private UserService userService;
     @Autowired
     private PostService postService;
+    @Autowired
+    private CategoryService categoryService;
+    @Autowired
+    private TagService tagService;
+    @Autowired
+    private HttpServletRequest request;
 
     @PostMapping("/update")
     public JsonResponse updateAdmin(@RequestBody UpdateAdminRequest updateAdminRequest) {
@@ -72,4 +85,80 @@ public class AdminController {
         return postService.deletePost(postId);
     }
 
+    @PostMapping("/category/create/{category}")
+    public JsonResponse createCategory(@PathVariable String category) {
+        return categoryService.createCategory(category);
+    }
+
+    public JsonResponse updateCategory(Integer categoryId, String categoryName){
+        return categoryService.updateCategory(categoryId, categoryName);
+    }
+
+    @GetMapping("/category/delete/{category}")
+    public JsonResponse deleteCategory(@PathVariable String category) {
+        return categoryService.deleteCategory(category);
+    }
+
+    @GetMapping("/tag/delete/{tag}")
+    public JsonResponse deleteTag(@PathVariable String tag) {
+        return tagService.deleteTag(tag);
+    }
+
+    @PostMapping("/upload")
+    public JsonResponse upload(@RequestBody MultipartFile file) {
+        if (file == null || file.isEmpty()) {
+            return new JsonResponse(Status.FILE_EMPTY);
+        }
+        try {
+            File uploadsDir = new File (ResourceUtils.getURL("classpath:").getPath());
+            System.out.println(uploadsDir);
+            if (!uploadsDir.exists()){
+                uploadsDir = new File("");
+            }
+            File upload = new File(uploadsDir.getAbsolutePath(), "static/imgs");
+            if (!upload.exists()) {
+                upload.mkdir();
+            }
+            String orgName = file.getOriginalFilename();
+            String filePath = upload.getPath() + "/" + orgName;
+            File dest = new File(filePath);
+
+            file.transferTo(dest);
+            return new JsonResponse(Status.SUCCESS);
+        } catch (IOException e) {
+            e.printStackTrace();
+            return new JsonResponse(Status.SERVER_ERROR);
+        }
+    }
+
+    @GetMapping("/files")
+    public JsonResponse displayFileNames() {
+        try {
+            File classPath = new File(ResourceUtils.getURL("classpath:").getPath());
+            File files = new File(classPath.getAbsolutePath(), "static/imgs");
+            List<String> fileNames = new ArrayList<>();
+
+            for (File f : files.listFiles()) {
+                fileNames.add(f.getName());
+            }
+
+            return new JsonResponse(Status.SUCCESS, fileNames);
+        } catch (IOException e) {
+            e.printStackTrace();
+            return new JsonResponse(Status.SERVER_ERROR);
+        }
+    }
+
+    @GetMapping("files/delete/{file}")
+    public JsonResponse delete(@PathVariable String file) {
+        try {
+            File classPath = new File(ResourceUtils.getURL("classpath:").getPath());
+            File files = new File(classPath.getAbsolutePath(), "static/imgs");
+            File fileToBeDeleted = new File (files + "/" + file);
+            return new JsonResponse(Status.SUCCESS, file);
+        } catch (IOException e) {
+            e.printStackTrace();
+            return new JsonResponse(Status.SERVER_ERROR);
+        }
+    }
 }
