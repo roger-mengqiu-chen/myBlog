83921024535a25a6791d965ff4a03266c2e8f73c
Roger Chen <mengqiu.chen@hotmail.com>
2022-01-21 18:18:50 -0700

    Use relative path for IMAGE, AVATAR

--- src/main/java/com/myblog/myblog/constant/Website.java
+++ src/main/java/com/myblog/myblog/constant/Website.java
@@ -3,6 +3,6 @@ package com.myblog.myblog.constant;
 public class Website {
 
     public final static String LINK = "http://localhost:8080";
-    public final static String IMAGE = LINK + "/static/imgs";
-    public final static String AVATAR = LINK + "/static/avatar";
+    public final static String IMAGE =  "/static/imgs";
+    public final static String AVATAR = "/static/avatar";
 }
