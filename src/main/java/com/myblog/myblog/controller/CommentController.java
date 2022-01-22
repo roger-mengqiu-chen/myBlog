d23b7cc19a197141c80a382b75c483aa6e976ad3
Roger Chen <mengqiu.chen@hotmail.com>
2022-01-21 18:49:12 -0700

    Delete comments should have permission

--- src/main/java/com/myblog/myblog/controller/CommentController.java
+++ src/main/java/com/myblog/myblog/controller/CommentController.java
@@ -1,21 +1,13 @@
 package com.myblog.myblog.controller;
 
-import com.myblog.myblog.constant.Status;
-import com.myblog.myblog.entity.Comment;
-import com.myblog.myblog.entity.User;
 import com.myblog.myblog.request.CommentRequest;
 import com.myblog.myblog.response.JsonResponse;
 import com.myblog.myblog.service.CommentService;
 import com.myblog.myblog.service.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.core.Authentication;
-import org.springframework.security.core.authority.SimpleGrantedAuthority;
 import org.springframework.security.core.context.SecurityContextHolder;
-import org.springframework.security.core.parameters.P;
 import org.springframework.web.bind.annotation.*;
-
-import java.util.ArrayList;
-import java.util.List;
 
 @RestController
 @RequestMapping("/comment")
@@ -49,21 +41,6 @@ public class CommentController {
     @GetMapping("/ofcomment{commentId}")
     public JsonResponse getRepliesOfComment(@PathVariable Long commentId) {
         return commentService.getChildCommentOfComment(commentId);
-    }
-
-    @GetMapping("/delete/{commentId}")
-    public JsonResponse deleteComment(@PathVariable Long commentId) {
-        Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
-        List<SimpleGrantedAuthority> authorities = new ArrayList(authentication.getAuthorities());
-        String role = authorities.get(0).getAuthority();
-        User user = userService.getUserByName(authentication.getName());
-        Comment comment = (Comment) commentService.getCommentById(commentId).getData();
-        if (role.equals("ROLE_ADMIN") || comment.getCommenterId().equals(user.getUserId())) {
-           return commentService.deleteComment(commentId);
-        }
-        else {
-            return new JsonResponse(Status.PERMISSION_DENIED);
-        }
     }
 
 }
