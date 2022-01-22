fd8627741a1d6592e7691522c955540397bbae1c
Roger Chen <mengqiu.chen@hotmail.com>
2022-01-21 17:15:55 -0700

    Add Image and avatar path

---
+++ src/main/java/com/myblog/myblog/constant/Website.java
@@ -0,0 +1,8 @@
+package com.myblog.myblog.constant;
+
+public class Website {
+
+    public final static String LINK = "http://localhost:8080";
+    public final static String IMAGE = LINK + "/static/imgs";
+    public final static String AVATAR = LINK + "/static/avatar";
+}
