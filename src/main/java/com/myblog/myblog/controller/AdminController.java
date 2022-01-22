ca0af82e637ec8792ed666a305b037b7a8df6bac
Roger Chen <mengqiu.chen@hotmail.com>
2022-01-21 18:58:16 -0700

    Remove unnecessary import

--- src/main/java/com/myblog/myblog/controller/AdminController.java
+++ src/main/java/com/myblog/myblog/controller/AdminController.java
@@ -10,9 +10,6 @@ import com.myblog.myblog.service.*;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.util.ResourceUtils;
 import org.springframework.web.bind.annotation.*;
-import org.springframework.web.multipart.MultipartFile;
-
-import javax.servlet.http.HttpServletRequest;
 import java.io.File;
 import java.io.IOException;
 import java.util.ArrayList;
